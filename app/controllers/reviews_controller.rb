ce8115a0990f3d3c320dd2ea1db745705de69f4e
Mylene Desroses <mylene.desroses@gmail.com>
2021-12-11 20:56:17 -0400

    ajout des reviews sur la restaurants/show + mise en page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,8 +5,8 @@ class ReviewsController < ApplicationController
   end
 
   def create
-    @review = Review.new(review_params)
     @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new(review_params)
     @review.restaurant = @restaurant
     @review.save
     redirect_to restaurant_path(@restaurant)
@@ -15,6 +15,6 @@ class ReviewsController < ApplicationController
   private
 
   def review_params
-    params.require(:review).permit(:content)
+    params.require(:review).permit(:content, :rating)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
-Restaurant.create(name: "Le Bistro des Augustins", address: "39 Quai des Grands Augustins, 75006 Paris", category: "french")
-Restaurant.create(name: "Au Rendez-Vous des Belges", address:"23 Rue de Dunkerque, 75010 Paris", category: "belgian")
-Restaurant.create(name: "Italian Trattoria", address:"11 Rue de la République, 69001 Lyon", category: "italian" )
-Restaurant.create(name: "Engimono", address: "8 Rue Servient, 69003 Lyon", category: "chinese")
-Restaurant.create(name: "Fuji Sushi-bar", address: "46 Rue Ernest Desproges, Fort-de-France 97200, Martinique", category: "japanese")
+Restaurant.destroy_all
+Restaurant.create(name: "Le Bistro des Augustins", address: "39 Quai des Grands Augustins, 75006 Paris", category: "french", phone_number: "0128357894")
+Restaurant.create(name: "Au Rendez-Vous des Belges", address:"23 Rue de Dunkerque, 75010 Paris", category: "belgian", phone_number: "0167397418")
+Restaurant.create(name: "Italian Trattoria", address:"11 Rue de la République, 69001 Lyon", category: "italian", phone_number: "0467797618")
+Restaurant.create(name: "Engimono", address: "8 Rue Servient, 69003 Lyon", category: "chinese", phone_number: "0464799618")
+Restaurant.create(name: "Fuji Sushi-bar", address: "46 Rue Ernest Desproges, Fort-de-France 97200, Martinique", category: "japanese", phone_number: "0596739418")
+Restaurant.create(name: "Le Bistro des Augustins", address: "39 Quai des Grands Augustins, 75006 Paris", category: "french", phone_number: "0128357894")
+Restaurant.create(name: "Au Rendez-Vous des Belges", address:"23 Rue de Dunkerque, 75010 Paris", category: "belgian", phone_number: "0167397418")
+Restaurant.create(name: "Italian Trattoria", address:"11 Rue de la République, 69001 Lyon", category: "italian", phone_number: "0467797618")
+Restaurant.create(name: "Engimono", address: "8 Rue Servient, 69003 Lyon", category: "chinese", phone_number: "0464799618")
+Restaurant.create(name: "Fuji Sushi-bar", address: "46 Rue Ernest Desproges, Fort-de-France 97200, Martinique", category: "japanese", phone_number: "0596739418")
